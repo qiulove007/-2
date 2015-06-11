d081a440c86a667ccfed2279ec625e71113c8e30
PC－qiu <pc-qiu@PCqiudeMBP.lan>
2015-06-11 15:53:48 +0800

    整理代码达到精简效果 将重复代码抽取到一个方法中：1.相同的代码放到一个方法中，2.不同代码变成参数，3.在使用到这段掉吗的地方调用这个方法，传递参数

--- 黑马微博2/Classes/AppDelegate.m
+++ 黑马微博2/Classes/AppDelegate.m
@@ -8,6 +8,10 @@
 
 #import "AppDelegate.h"
 #import "PrefixHeader.pch"
+#import "HWHomeViewController.h"
+#import "HWMessageCenterViewController.h"
+#import "HWDiscoverViewController.h"
+#import "HWProfileViewController.h"
 
 @interface AppDelegate ()
 
@@ -28,51 +32,24 @@
     UITabBarController* tabbar=[[UITabBarController alloc]init];
     self.window.rootViewController = tabbar;
     
-    //3.设置子控制器
-    UIViewController* vc1=[[UIViewController alloc]init];
-    vc1.tabBarItem.title=@"首页";
-    vc1.tabBarItem.image = [[UIImage imageNamed:@"1"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    vc1.tabBarItem.selectedImage = [[UIImage imageNamed:@"5"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    vc1.view.backgroundColor = RandomColor;
-    //设置未选中文字的样式
-    NSMutableDictionary* textAttrs=[NSMutableDictionary dictionary];
-    textAttrs[NSForegroundColorAttributeName] = [UIColor blackColor];
-    [vc1.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
-    //设置选中文字的样式
-    NSMutableDictionary* seltextAttrs=[NSMutableDictionary dictionary];
-    seltextAttrs[NSForegroundColorAttributeName] = [UIColor orangeColor];
-    [vc1.tabBarItem setTitleTextAttributes:seltextAttrs forState:UIControlStateSelected];
+    //设置子控制器
+    HWHomeViewController* home=[[HWHomeViewController alloc]init];
+    [self addChildVC:home title:@"首页" image:@"1" selectedImage:@"5"];
     
+    HWMessageCenterViewController* msg=[[HWMessageCenterViewController alloc]init];
+    [self addChildVC:msg title:@"消息" image:@"2" selectedImage:@"6"];
     
-    UIViewController* vc2=[[UIViewController alloc]init];
-    vc2.tabBarItem.title=@"消息";
-    vc2.tabBarItem.image = [[UIImage imageNamed:@"2.jpg"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    vc2.tabBarItem.selectedImage = [[UIImage imageNamed:@"6.jpg"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    vc2.view.backgroundColor = RandomColor;
-    [vc2.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
-    [vc2.tabBarItem setTitleTextAttributes:seltextAttrs forState:UIControlStateSelected];
+    HWDiscoverViewController* discover=[[HWDiscoverViewController alloc]init];
+    [self addChildVC:discover title:@"发现" image:@"3" selectedImage:@"1"];
     
-    UIViewController* vc3=[[UIViewController alloc]init];
-    vc3.tabBarItem.title=@"发现";
-    vc3.tabBarItem.image = [[UIImage imageNamed:@"3.jpg"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    vc3.tabBarItem.selectedImage = [[UIImage imageNamed:@"1.jpg"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    vc3.view.backgroundColor = RandomColor;
-    [vc3.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
-    [vc3.tabBarItem setTitleTextAttributes:seltextAttrs forState:UIControlStateSelected];
+    HWProfileViewController* profile=[[HWProfileViewController alloc]init];
+    [self addChildVC:profile title:@"我" image:@"4" selectedImage:@"2"];
     
-    UIViewController* vc4=[[UIViewController alloc]init];
-    vc4.tabBarItem.title=@"我";
-    vc4.tabBarItem.image = [[UIImage imageNamed:@"4.jpg"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    vc4.tabBarItem.selectedImage = [[UIImage imageNamed:@"2.jpg"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    vc4.view.backgroundColor = RandomColor;
-    [vc4.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
-    [vc4.tabBarItem setTitleTextAttributes:seltextAttrs forState:UIControlStateSelected];
-    //注意：如果直接给一张图片会被渲染成默认的颜色，所以要设置为不要被渲染的模式
-    //即imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal
     
     
     //4.添加子控制器
-    tabbar.viewControllers =@[vc1,vc2,vc3,vc4];
+    tabbar.viewControllers =@[home,msg,discover,profile];
+    
     
     
     
@@ -83,6 +60,27 @@
     
     return YES;
 }
+
+
+-(void)addChildVC:(UIViewController*)childVc title:(NSString*)title image:(NSString*)image selectedImage:(NSString*)selectedImage
+{
+    childVc.tabBarItem.title=title;
+    childVc.tabBarItem.image = [[UIImage imageNamed:image] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    childVc.tabBarItem.selectedImage = [[UIImage imageNamed:selectedImage] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    childVc.view.backgroundColor = RandomColor;
+    //设置未选中文字的样式
+    NSMutableDictionary* textAttrs=[NSMutableDictionary dictionary];
+    textAttrs[NSForegroundColorAttributeName] = [UIColor blackColor];
+    [childVc.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
+    //设置选中文字的样式
+    NSMutableDictionary* seltextAttrs=[NSMutableDictionary dictionary];
+    seltextAttrs[NSForegroundColorAttributeName] = [UIColor orangeColor];
+    [childVc.tabBarItem setTitleTextAttributes:seltextAttrs forState:UIControlStateSelected];
+    
+    //注意：如果直接给一张图片会被渲染成默认的颜色，所以要设置为不要被渲染的模式
+    //即imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal
+}
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -106,4 +104,48 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+
+
+/*//3.设置子控制器
+ UIViewController* vc1=[[UIViewController alloc]init];
+ vc1.tabBarItem.title=@"首页";
+ vc1.tabBarItem.image = [[UIImage imageNamed:@"1"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ vc1.tabBarItem.selectedImage = [[UIImage imageNamed:@"5"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ vc1.view.backgroundColor = RandomColor;
+ //设置未选中文字的样式
+ NSMutableDictionary* textAttrs=[NSMutableDictionary dictionary];
+ textAttrs[NSForegroundColorAttributeName] = [UIColor blackColor];
+ [vc1.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
+ //设置选中文字的样式
+ NSMutableDictionary* seltextAttrs=[NSMutableDictionary dictionary];
+ seltextAttrs[NSForegroundColorAttributeName] = [UIColor orangeColor];
+ [vc1.tabBarItem setTitleTextAttributes:seltextAttrs forState:UIControlStateSelected];
+ 
+ 
+ UIViewController* vc2=[[UIViewController alloc]init];
+ vc2.tabBarItem.title=@"消息";
+ vc2.tabBarItem.image = [[UIImage imageNamed:@"2.jpg"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ vc2.tabBarItem.selectedImage = [[UIImage imageNamed:@"6.jpg"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ vc2.view.backgroundColor = RandomColor;
+ [vc2.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
+ [vc2.tabBarItem setTitleTextAttributes:seltextAttrs forState:UIControlStateSelected];
+ 
+ UIViewController* vc3=[[UIViewController alloc]init];
+ vc3.tabBarItem.title=@"发现";
+ vc3.tabBarItem.image = [[UIImage imageNamed:@"3.jpg"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ vc3.tabBarItem.selectedImage = [[UIImage imageNamed:@"1.jpg"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ vc3.view.backgroundColor = RandomColor;
+ [vc3.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
+ [vc3.tabBarItem setTitleTextAttributes:seltextAttrs forState:UIControlStateSelected];
+ 
+ UIViewController* vc4=[[UIViewController alloc]init];
+ vc4.tabBarItem.title=@"我";
+ vc4.tabBarItem.image = [[UIImage imageNamed:@"4.jpg"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ vc4.tabBarItem.selectedImage = [[UIImage imageNamed:@"2.jpg"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ vc4.view.backgroundColor = RandomColor;
+ [vc4.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
+ [vc4.tabBarItem setTitleTextAttributes:seltextAttrs forState:UIControlStateSelected];
+ //注意：如果直接给一张图片会被渲染成默认的颜色，所以要设置为不要被渲染的模式
+ //即imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal
+ */
 @end
