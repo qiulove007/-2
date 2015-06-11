579ab6eb2c7c90219eb4ecf4886a65ce9cc5acdd
PC－qiu <pc-qiu@PCqiudeMBP.lan>
2015-06-11 15:52:00 +0800

    添加首页消息我发现这4个tableviewcontroller

---
+++ 黑马微博2/Classes/HWProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  HWProfileViewController.h
+//  黑马微博2
+//
+//  Created by PC－qiu on 15/6/11.
+//  Copyright (c) 2015年 HM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HWProfileViewController : UITableViewController
+
+@end
