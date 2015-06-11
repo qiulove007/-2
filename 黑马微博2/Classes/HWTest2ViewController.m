fd77fcecd8ba0ee00c3b9fb407038a6152902be8
PC－qiu <pc-qiu@PCqiudeMBP.lan>
2015-06-11 22:35:31 +0800

    导航栏的两个按钮和对应功能

--- 黑马微博2/Classes/HWTest2ViewController.m
+++ 黑马微博2/Classes/HWTest2ViewController.m
@@ -17,6 +17,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    UIButton* back=[UIButton buttonWithType:UIButtonTypeCustom];//创建一个自定义的按钮
+    [back addTarget:self action:@selector(toBack:) forControlEvents:UIControlEventTouchUpInside];
+    [back setBackgroundImage:[UIImage imageNamed:@"1"] forState:UIControlStateNormal];
+    [back setBackgroundImage:[UIImage imageNamed:@"3"] forState:UIControlStateHighlighted];
+    back.size = back.currentBackgroundImage.size;//如果没有正常显示，请查看有没有设置大小
+    self.navigationItem.leftBarButtonItem=[[UIBarButtonItem alloc] initWithCustomView:back];
+    
+    UIButton* home=[UIButton buttonWithType:UIButtonTypeCustom];//创建一个自定义的按钮
+    [home addTarget:self action:@selector(toHome:) forControlEvents:UIControlEventTouchUpInside];
+    [home setBackgroundImage:[UIImage imageNamed:@"2"] forState:UIControlStateNormal];
+    [home setBackgroundImage:[UIImage imageNamed:@"4"] forState:UIControlStateHighlighted];
+    home.size=home.currentBackgroundImage.size;//如果没有正常显示，请查看有没有设置大小
+    self.navigationItem.rightBarButtonItem=[[UIBarButtonItem alloc]initWithCustomView:home];
+}
+
+-(void)toBack:(id)sender
+{
+    [self.navigationController popViewControllerAnimated:YES];//返回上一个viewController
+}
+
+-(void)toHome:(id)sender
+{
+    [self.navigationController popToRootViewControllerAnimated:YES];//返回根视图viewController
 }
 
 - (void)didReceiveMemoryWarning {
