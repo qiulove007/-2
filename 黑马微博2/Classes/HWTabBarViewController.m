ea683343a417c23b01fba6bb4de733521998b880
PC－qiu <pc-qiu@PCqiudeMBP.lan>
2015-06-11 20:27:25 +0800

    将添加控件放入方法中

--- 黑马微博2/Classes/HWTabBarViewController.m
+++ 黑马微博2/Classes/HWTabBarViewController.m
@@ -19,6 +19,7 @@
     
     //设置子控制器
     HWHomeViewController* home=[[HWHomeViewController alloc]init];
+    
     [self addChildVC:home title:@"首页" image:@"1" selectedImage:@"5"];
     
     HWMessageCenterViewController* msg=[[HWMessageCenterViewController alloc]init];
@@ -31,7 +32,7 @@
     [self addChildVC:profile title:@"我" image:@"4" selectedImage:@"2"];
     
     //4.添加子控制器
-    self.viewControllers =@[home,msg,discover,profile];
+    //self.viewControllers =@[home,msg,discover,profile];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -41,7 +42,10 @@
 
 -(void)addChildVC:(UIViewController*)childVc title:(NSString*)title image:(NSString*)image selectedImage:(NSString*)selectedImage
 {
+    childVc.navigationItem.title=title;//设置导航标题与下标题一致
     childVc.tabBarItem.title=title;
+    //childVc.title=title;//这一句可以完成上面两句代码的作用
+    
     childVc.tabBarItem.image = [[UIImage imageNamed:image] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     childVc.tabBarItem.selectedImage = [[UIImage imageNamed:selectedImage] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     childVc.view.backgroundColor = RandomColor;
@@ -56,8 +60,56 @@
     
     //注意：如果直接给一张图片会被渲染成默认的颜色，所以要设置为不要被渲染的模式
     //即imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal
+    
+    UINavigationController* nav=[[UINavigationController alloc] initWithRootViewController:childVc];
+    
+    [self addChildViewController:nav];
 }
 
+
+
+/*//3.设置子控制器原始代码，未抽取方法
+ UIViewController* vc1=[[UIViewController alloc]init];
+ vc1.tabBarItem.title=@"首页";
+ vc1.tabBarItem.image = [[UIImage imageNamed:@"1"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ vc1.tabBarItem.selectedImage = [[UIImage imageNamed:@"5"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ vc1.view.backgroundColor = RandomColor;
+ //设置未选中文字的样式
+ NSMutableDictionary* textAttrs=[NSMutableDictionary dictionary];
+ textAttrs[NSForegroundColorAttributeName] = [UIColor blackColor];
+ [vc1.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
+ //设置选中文字的样式
+ NSMutableDictionary* seltextAttrs=[NSMutableDictionary dictionary];
+ seltextAttrs[NSForegroundColorAttributeName] = [UIColor orangeColor];
+ [vc1.tabBarItem setTitleTextAttributes:seltextAttrs forState:UIControlStateSelected];
+ 
+ 
+ UIViewController* vc2=[[UIViewController alloc]init];
+ vc2.tabBarItem.title=@"消息";
+ vc2.tabBarItem.image = [[UIImage imageNamed:@"2.jpg"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ vc2.tabBarItem.selectedImage = [[UIImage imageNamed:@"6.jpg"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ vc2.view.backgroundColor = RandomColor;
+ [vc2.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
+ [vc2.tabBarItem setTitleTextAttributes:seltextAttrs forState:UIControlStateSelected];
+ 
+ UIViewController* vc3=[[UIViewController alloc]init];
+ vc3.tabBarItem.title=@"发现";
+ vc3.tabBarItem.image = [[UIImage imageNamed:@"3.jpg"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ vc3.tabBarItem.selectedImage = [[UIImage imageNamed:@"1.jpg"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ vc3.view.backgroundColor = RandomColor;
+ [vc3.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
+ [vc3.tabBarItem setTitleTextAttributes:seltextAttrs forState:UIControlStateSelected];
+ 
+ UIViewController* vc4=[[UIViewController alloc]init];
+ vc4.tabBarItem.title=@"我";
+ vc4.tabBarItem.image = [[UIImage imageNamed:@"4.jpg"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ vc4.tabBarItem.selectedImage = [[UIImage imageNamed:@"2.jpg"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ vc4.view.backgroundColor = RandomColor;
+ [vc4.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
+ [vc4.tabBarItem setTitleTextAttributes:seltextAttrs forState:UIControlStateSelected];
+ //注意：如果直接给一张图片会被渲染成默认的颜色，所以要设置为不要被渲染的模式
+ //即imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal
+ */
 /*
 #pragma mark - Navigation
 
