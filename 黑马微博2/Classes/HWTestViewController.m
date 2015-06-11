aba14e57d6e17a9d92b62c506216bdaee3cbe989
PC－qiu <pc-qiu@PCqiudeMBP.lan>
2015-06-11 20:54:01 +0800

    测试页面，当点击tableviewcontroller的内容时将要跳转的页面

---
+++ 黑马微博2/Classes/HWTestViewController.m
@@ -0,0 +1,37 @@
+//
+//  HWTestViewController.m
+//  黑马微博2
+//
+//  Created by PC－qiu on 15/6/11.
+//  Copyright (c) 2015年 HM. All rights reserved.
+//
+
+#import "HWTestViewController.h"
+
+@interface HWTestViewController ()
+
+@end
+
+@implementation HWTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
