d89e63bd66d80e5f3a37db7a791f1d94e5d72e63
PC－qiu <pc-qiu@PCqiudeMBP.lan>
2015-06-11 21:54:35 +0800

    扩展UIView实现坐标和宽度高度的直接读取设置

---
+++ 黑马微博2/Classes/Extension.h
@@ -0,0 +1,18 @@
+//
+//  Extension.h
+//  黑马微博2
+//
+//  Created by PC－qiu on 15/6/11.
+//  Copyright (c) 2015年 HM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView(Extension)
+@property (nonatomic,assign)CGFloat x;
+@property (nonatomic,assign)CGFloat y;
+@property (nonatomic,assign)CGFloat width;
+@property (nonatomic,assign)CGFloat height;
+@property (nonatomic,assign)CGSize size;
+@property (nonatomic,assign)CGPoint origin;
+@end
