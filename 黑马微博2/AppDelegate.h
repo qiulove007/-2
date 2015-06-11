b103c2ad92d3329b60eefd1a8fe904e47d1527f1
PC－qiu <pc-qiu@PCqiudeMBP.lan>
2015-06-11 11:23:10 +0800

    Initial Commit

---
+++ 黑马微博2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  黑马微博2
+//
+//  Created by PC－qiu on 15/6/11.
+//  Copyright (c) 2015年 HM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
