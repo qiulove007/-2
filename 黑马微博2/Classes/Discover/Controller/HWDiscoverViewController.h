ea45230f84f60a7d791e0679d8792f696329bd3e
PC－qiu <pc-qiu@PCqiudeMBP.lan>
2015-06-12 14:49:11 +0800

    分层

---
+++ 黑马微博2/Classes/Discover/Controller/HWDiscoverViewController.h
@@ -0,0 +1,13 @@
+//
+//  HWDiscoverViewController.h
+//  黑马微博2
+//
+//  Created by PC－qiu on 15/6/11.
+//  Copyright (c) 2015年 HM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HWDiscoverViewController : UITableViewController
+
+@end
---
+++ 黑马微博2/Classes/Main/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  黑马微博2
+//
+//  Created by PC－qiu on 15/6/11.
+//  Copyright (c) 2015年 HM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
