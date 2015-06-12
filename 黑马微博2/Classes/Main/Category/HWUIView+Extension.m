dbd2a280234db5424da68d413b1b4398f25b8044
PC－qiu <pc-qiu@PCqiudeMBP.lan>
2015-06-12 15:39:46 +0800

    命名规范

---
+++ 黑马微博2/Classes/Main/Category/HWUIView+Extension.m
@@ -0,0 +1,82 @@
+//
+//  Extension.m
+//  黑马微博2
+//
+//  Created by PC－qiu on 15/6/11.
+//  Copyright (c) 2015年 HM. All rights reserved.
+//
+
+#import "Extension.h"
+
+@implementation UIView(Extension)
+
+-(void)setX:(CGFloat)x
+{
+    CGRect frame=self.frame;
+    frame.origin.x=x;
+    self.frame=frame;
+}
+-(CGFloat)x
+{
+    return self.frame.origin.x;
+}
+
+-(void)setY:(CGFloat)y
+{
+    CGRect frame=self.frame;
+    frame.origin.y=y;
+    self.frame=frame;
+}
+-(CGFloat)y
+{
+    return self.frame.origin.y;
+}
+
+-(void)setWidth:(CGFloat)width
+{
+    CGRect frame=self.frame;
+    frame.size.width=width;
+    self.frame=frame;
+}
+-(CGFloat)width
+{
+    return self.frame.size.width;
+}
+
+-(void)setHeight:(CGFloat)height
+{
+    CGRect frame=self.frame;
+    frame.size.height=height;
+    self.frame=frame;
+}
+-(CGFloat)height
+{
+    return self.frame.size.height;
+}
+
+-(void)setSize:(CGSize)size
+{
+    CGRect frame=self.frame;
+    frame.size.width=size.width;
+    frame.size.height=size.height;
+    self.frame=frame;
+}
+
+-(CGSize)size
+{
+    return self.frame.size;
+}
+
+-(void)setOrigin:(CGPoint)origin
+{
+    CGRect frame=self.frame;
+    frame.origin.x=origin.x;
+    frame.origin.y=origin.y;
+    self.frame=frame;
+}
+-(CGPoint)origin
+{
+    return self.frame.origin;
+}
+
+@end
