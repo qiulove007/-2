6dd09bf856775829d8426a4549aee0ff6c4b55a2
PC－qiu <pc-qiu@PCqiudeMBP.lan>
2015-06-12 19:55:34 +0800

    统一导航栏普通按钮主题

--- 黑马微博2/Classes/MessageCenter/Controller/HWMessageCenterViewController.m
+++ 黑马微博2/Classes/MessageCenter/Controller/HWMessageCenterViewController.m
@@ -18,11 +18,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.navigationItem.rightBarButtonItem=[[UIBarButtonItem alloc] initWithTitle:@"写私信" style:UIBarButtonItemStylePlain target:self action:@selector(composeMsg)];
+    //style在IOS7之后显示没有效果
+}
+
+-(void)composeMsg
+{
+    NSLog(@"composeMsg");
 }
 
 #pragma mark - Table view data source
