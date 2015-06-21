b1632a944ef1eb5bbb3b8d6b826721c0634db518
PC－qiu <pc-qiu@PCqiudeMacBook-Pro.local>
2015-06-21 16:54:14 +0800

    标题封装

--- 黑马微博2/Classes/Home/Controller/HWHomeViewController.m
+++ 黑马微博2/Classes/Home/Controller/HWHomeViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "HWHomeViewController.h"
+#import "AFNetworking.h"
+#import "HWTitleButton.h"
 
 @interface HWHomeViewController ()
 
@@ -17,29 +19,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithTarget:self Action:@selector(toFriend) image:@"5" highImage:@"6"];
+    //设置导航栏内容
+    [self setControllerContent];
     
-    self.navigationItem.rightBarButtonItem = [UIBarButtonItem itemWithTarget:self Action:@selector(toPop) image:@"d_nu" highImage:@"1"];
-    
-    
-    //添加标题按钮
-    UIButton* titleButton=[UIButton buttonWithType:UIButtonTypeCustom];
-    titleButton.width=150;
-    titleButton.height=30;
-    titleButton.backgroundColor = RandomColor;
-    
-    //设置按钮的图片和文字
-    [titleButton setTitle:@"首页" forState:UIControlStateNormal];
-    [titleButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    titleButton.titleLabel.font=[UIFont boldSystemFontOfSize:17];
-    [titleButton setImage:[UIImage imageNamed:@"listDown"] forState:UIControlStateNormal];
-    titleButton.imageEdgeInsets =UIEdgeInsetsMake(0,70, 0, 0);
-    titleButton.titleEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 40);
-    
-    //监听标题点击
-    [titleButton addTarget:self action:@selector(titleClick:) forControlEvents:UIControlEventTouchUpInside];
-    
-    self.navigationItem.titleView = titleButton;//设置导航栏的标题视图为一个按钮
+    //获得用户信息
+    [self setUserInfo];
 }
 
 /**
@@ -66,9 +50,59 @@
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)setUserInfo
+{
+    //请求地址为：https://api.weibo.com/2/users/show.json
+    AFHTTPRequestOperationManager* manager=[AFHTTPRequestOperationManager manager];
+    
+    //拼接请求参数
+    HWAccount* acc=[HWAccountTool account];
+    NSMutableDictionary* params=[NSMutableDictionary dictionary];
+    params[@"access_token"]=acc.access_token;
+    params[@"uid"]= acc.uid;
+    
+    //发送请求，要求发送get请求
+    [manager GET:@"https://api.weibo.com/2/users/show.json" parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        //HWLog(@"请求用户信息成功--%@",responseObject);
+        
+        //获取姓名
+        NSString* name=[responseObject[@"name"] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        [((UIButton*)self.navigationItem.titleView) setTitle:name forState:UIControlStateNormal];
+        //保存昵称进沙盒
+        acc.name=name;
+        [HWAccountTool saveAccount:acc];
+        
+        HWLog(@"%@",name);
+        
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        HWLog(@"请求用户信息失败");
+    }];
+}
+
+//设置导航栏内容
+-(void) setControllerContent
+{
+    self.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithTarget:self Action:@selector(toFriend) image:@"5" highImage:@"6"];
+    
+    self.navigationItem.rightBarButtonItem = [UIBarButtonItem itemWithTarget:self Action:@selector(toPop) image:@"d_nu" highImage:@"1"];
+    
+    
+    //添加标题按钮
+    HWTitleButton* titleButton=[HWTitleButton buttonWithType:UIButtonTypeCustom];
+//    titleButton.width=250;
+//    titleButton.height=30;
+    
+    //设置按钮的图片和文字
+    NSString* name=[HWAccountTool account].name;
+    [titleButton setTitle:name?name:@"" forState:UIControlStateNormal];
+    
+    
+    //监听标题点击
+    [titleButton addTarget:self action:@selector(titleClick:) forControlEvents:UIControlEventTouchUpInside];
+    //[titleButton sizeToFit];//内容有多少，按钮就有多大，autosize类似
+    
+    self.navigationItem.titleView = titleButton;//设置导航栏的标题视图为一个按钮
 }
 
 #pragma mark - Table view data source
